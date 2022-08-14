5702e0f5fa79bab04de084b1e4899527df178f08
mustafatumertan <mustafatumertan@gmail.com>
2022-08-14 14:57:39 +0300

    Several Studies on React State, Props and Hooks

--- pages/ComponentRepo.js
+++ pages/ComponentRepo.js
@@ -1,10 +1,37 @@
+import React, {useEffect} from 'react';
 
-export function CompA() {
+const ArrowFunction = () => {
+  return (
+    <div>
+      <h1>I am arrow function</h1>
+    </div>
+  );
+}
+
+const ArrowFunction2 = _ =>
+  <div>
+  <h1>I am arrow function 2</h1>
+  </div>
+
+export function CompA(props) {
+    
+    const {myProp1, myProp2, myProp3, MyProp4, myProp5} = props;
+    
+    useEffect(() => {
+      console.log("Component A Use Effect");
+    })
+
     return (
       <>
         <h1>Component A</h1>
         <p>Hello Component A</p>
-        <CompB/>
+        <div>My Prop1: {myProp1}</div>
+        <div>My Prop2: {myProp2}</div>
+        <div>My Prop3: {myProp3.toString()}</div>
+        <div>My Prop4: {<MyProp4/>}</div>
+        <div>My Prop5: {JSON.stringify(myProp5)}</div>
+
+
       </>
     )
   }
@@ -16,4 +43,35 @@ export function CompA() {
         <p>Hello Component B</p>
       </>
     )
+  }
+
+  export class CompC extends React.Component {
+
+    constructor() {
+      super();
+
+      this.state = {
+        myValue: 10
+      }
+    }
+
+    changeState(newVal) {
+      this.setState({
+        myValue: newVal
+      });
+    }
+
+
+    render() {
+      
+      const {myValue} = this.state;
+      return (
+        <>
+          <h1>Component C</h1>
+          Current Value: <h1>{this.state.myValue}</h1>
+          <button onClick={() => this.changeState(myValue + 1)}>+</button>
+			    <button onClick={() => this.changeState(myValue - 1)}>-</button>
+        </>
+      );
+    }
   }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,38 @@
-import {CompA} from './ComponentRepo';
+import {CompA, CompC} from './ComponentRepo';
+import React, {useState, useEffect} from 'react'
 
-export default function Home() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <CompA/>     
-    </div>
+function Home() {
 
-  )
+	// [stateValue, mutateState]
+	// const stateValue = useState("mustafa");
+	const [stateValue, setStateValue] = useState(10);
+	const [otherValue, setOtherValue] = useState(100);
+
+	useEffect(() => {
+		console.log("Use Effect Called for Other Value");
+	}, [])
+	
+
+  	return (
+   	<div>
+   		Value: <h1>{stateValue}</h1>
+			<button onClick={() => setStateValue(stateValue + 1)}>+</button>
+			<button onClick={() => setStateValue(stateValue - 1)}>-</button>
+			<hr/>
+			Other Value: <h1>{otherValue}</h1>
+			<button onClick={() => setOtherValue(otherValue + 10)}>+</button>
+			<button onClick={() => setOtherValue(otherValue - 10)}>-</button>
+			<hr/>
+			<CompA 
+				myProp1={stateValue}
+				myProp2="My Custom Value"
+				myProp3={true}
+				MyProp4={() => <div>New JSX Passed as a Prop</div>}
+				myProp5={{item1: 2, item2: 7}}
+			/>
+   	</div>
+
+  	)
 }
+
+export default Home;
